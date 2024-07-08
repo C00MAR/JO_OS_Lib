ee5c6d7af966803aa7b3bef555025099e0926481
C00MAR <moureau.marc78@gmail.com>
2024-07-08 18:57:54 +0200

    :tada: fixEsLint Error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,6 @@ import globals from "globals";
 
 
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
-  {languageOptions: { globals: globals.node }},
-  {"parserOptions": {"ecmaVersion": 2020,"sourceType": "module"}},
+  { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
+  { languageOptions: { globals: globals.node }, parserOptions: { ecmaVersion: 2020, sourceType: "module" } },
 ];
