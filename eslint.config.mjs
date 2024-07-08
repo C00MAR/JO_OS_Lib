92a93f94b6d59fd871f943cb7b661fcc01823d9b
C00MAR <moureau.marc78@gmail.com>
2024-07-08 18:37:19 +0200

    :rocket: feature/areaToSport

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,4 +4,5 @@ import globals from "globals";
 export default [
   {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
   {languageOptions: { globals: globals.node }},
+  {"parserOptions": {"ecmaVersion": 2020,"sourceType": "module"}},
 ];
---
+++ src/areaToSport.js
@@ -0,0 +1,26 @@
+/**
+ * Retrieves the sports associated with a given spot from the Paris 2024 competition sites API.
+ * @param {string} spot - The name of the spot.
+ * @returns {Promise<Array<string>>} - A promise that resolves to an array of sports associated with the city.
+ * @throws {Error} - If there is an error fetching the competition sites or parsing the response.
+ */
+export default async function areaToSport(spot) {
+    const apiUrl = `https://data.paris2024.org/api/explore/v2.1/catalog/datasets/paris-2024-sites-de-competition/records?where=%22${spot}%22`;
+    let sports = [];
+
+    try {
+        const response = await fetch(apiUrl);
+        var data = await response.json();
+    } catch (error) {
+        console.error('Error fetching competition sites:', error);
+        throw error;
+    }
+
+    data.results.forEach(event => {
+        if (event.nom_site == spot) {
+            sports.push(event.sports);
+        }
+    });
+
+    return sports;
+}
